d8b37c0b9fd42d7a8d22256d5e5c0381aaf90d9f
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2023-08-29 08:40:01 +0530

    component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import EventList from './components/EventList';
-import SearchBar from './components/SearchBar';
-import ParentComponentDropdown from './components/ParentComponentDropdown';
+import Header from './components/Header';
 
 const eventsData = [
   // Event data here...
@@ -11,32 +10,33 @@ const eventsData = [
     title: 'Event 1',
     date: 'August 25, 2023',
     location: 'Venue A',
-    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
   {
     id: 2,
     title: 'Event 2',
     date: 'September 5, 2023',
     location: 'Venue B',
-    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: ' consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
   {
     id: 3,
     title: 'Event 2',
     date: 'September 5, 2023',
     location: 'Venue B',
-    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: ' consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
   {
     id: 4,
     title: 'Event 2',
     date: 'September 5, 2023',
     location: 'Venue B',
-    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   }
 ];
 
 const App = () => {
+
   const [filteredEvents, setFilteredEvents] = useState(eventsData);
 
   const handleSearch = searchTerm => {
@@ -48,34 +48,32 @@ const App = () => {
 
   return (
     <div className="app">
+      
+      <Header/>
+      <div className="body-container">
 
-      <div className="header">
-        <h1>All Events</h1>
-        <ParentComponentDropdown />
-        <SearchBar onChange={handleSearch} />
-      </div>
-      <hr />
+        <div className="all-events">
+          <h2>All Events</h2>
+          <EventList events={filteredEvents} />
+          <EventList events={filteredEvents} />
+          <EventList events={filteredEvents} />
+        </div>
 
-      <div className="all-events">
-        <h2>All Events</h2>
-        <EventList events={filteredEvents} />
-        <EventList events={filteredEvents} />
-        <EventList events={filteredEvents} />
-      </div>
+        <div className="upcoming-events">
+          <h2>Upcoming Events</h2>
+          <EventList events={filteredEvents} />
+        </div>
 
-      <div className="upcoming-events">
-        <h2>Upcoming Events</h2>
-        <EventList events={filteredEvents} />
-      </div>
+        <div className="past-events">
+          <h2>Past Events</h2>
+          <EventList events={filteredEvents} />
+        </div>
 
-      <div className="past-events">
-        <h2>Past Events</h2>
-        <EventList events={filteredEvents} />
-      </div>
-
-      <div className="live-events">
-        <h2>Live Events</h2>
-        <EventList events={filteredEvents} />
+        <div className="live-events">
+          <h2>Live Events</h2>
+          <EventList events={filteredEvents} />
+        </div>
+        
       </div>
 
       

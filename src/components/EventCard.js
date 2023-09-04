51b6d5a75ec3e0a140c6b67fa9b5e548c171fc0c
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2023-09-04 22:16:12 +0530

    css evencard

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -2,16 +2,26 @@ import React from 'react';
 import '../styles/EventCard.css';
 
 const EventCard = ({ event }) => (
-  <div className="event-card">
-    <img src={process.env.PUBLIC_URL + '/images/event-image.jpg'} alt="Event" />
-    <hr />
-    <h3>{event.name}</h3>
-    <p>Date: {event.date}</p>
-    <p>Location: {event.location}</p>
-    <p>Start Time: {event.startTime}</p>
-    <p>End Time: {event.endTime}</p>
-    {/* <p>Description: {event.description}</p> */}
-    {/* <button>Resister</button> */}
+  <div className="event-card-container">
+
+    <div className="event-card">
+        <img src={process.env.PUBLIC_URL + '/images/event-image.jpg'} alt="Event" />
+        <hr />
+        <h3>{event.name}</h3>
+        <p>Date: {event.date}</p>
+        <p>Location: {event.location}</p>
+        <p>Start Time: {event.startTime}</p>
+        <p>End Time: {event.endTime}</p>
+    </div>
+        {/* <p>Description: {event.description}</p> */}
+        {/* <button>Resister</button> */}
+    <div className="register-block">
+      <p className="register-link">
+        <a href="#">Register Here</a>
+      </p>
+    </div>
+      
+      
   </div>
 );
 

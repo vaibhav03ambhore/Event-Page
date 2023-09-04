16d4ce01075a2723c6babfe253da7a7d50070ec9
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2023-09-04 23:31:46 +0530

    styles added

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -2,26 +2,27 @@ import React from 'react';
 import '../styles/EventCard.css';
 
 const EventCard = ({ event }) => (
-  <div className="event-card-container">
+  <div className="event-card">
 
-    <div className="event-card">
+    <div className="event-card-image">
         <img src={process.env.PUBLIC_URL + '/images/event-image.jpg'} alt="Event" />
-        <hr />
+    </div>
+
+    <div className="event-card-content">
         <h3>{event.name}</h3>
         <p>Date: {event.date}</p>
         <p>Location: {event.location}</p>
         <p>Start Time: {event.startTime}</p>
         <p>End Time: {event.endTime}</p>
-    </div>
+    
         {/* <p>Description: {event.description}</p> */}
         {/* <button>Resister</button> */}
-    <div className="register-block">
-      <p className="register-link">
-        <a href="#">Register Here</a>
-      </p>
+      <div className="register-block">
+        <span class="resister-text">
+          <a href="#">Register Here</a>
+        </span>
+      </div>
     </div>
-      
-      
   </div>
 );
 
